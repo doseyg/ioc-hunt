22466a5ead365e18701c0781b7f2a0326849fb80
Glen Dosey <doseyg@r-networks.net>
2018-10-17 00:56:15 -0400

    Requires Powershell 3 for TLS 1.2

--- ioc_updaters/virusshare.ps1
+++ ioc_updaters/virusshare.ps1
@@ -1,16 +1,17 @@
+## VirusShare (as of 2018) requires TLS 1.2 to download the hashes. TLS 1.2 requires PowerShell version 3. Therefore this script now requires PowerShell Version 3
 $source = "www.virusshare.com"
 $type = "malicious"
 
 $first_hashset = 0
-$last_hashset = 270
+$last_hashset = 337
 
 ## Get configuration from XML file
 [xml]$Config = Get-Content "config.ioc-hunt.xml"
 
 
 $conn = New-Object System.Data.SqlClient.SqlConnection
-#$conn.ConnectionString = "Data Source=tcp:IP;Database=HashDB;Integrated Security=false;UID=hash_user;Password=password;"
-$conn.ConnectionString = $Config.Settings.Global.SqlConnectionString
+#$conn.ConnectionString = "Data Source=tcp:172.16.72.131;Database=IOC-Hunt;Integrated Security=false;UID=hash_user;Password=Password1;"
+$conn.ConnectionString = $Config.Settings.Global.SqlConnectString
 $conn.open()
 $cmd = New-Object System.Data.SqlClient.SqlCommand
 $cmd.connection = $conn
@@ -19,12 +20,17 @@ for ($i = $first_hashset; $i -le $last_hashset; $i++) {
 
     $set = $i.ToString("00000")
     echo "Working on $source $set"
-    $request = [System.Net.WebRequest]::Create("https://virusshare.com/hashes/VirusShare_$set.md5"); 
-    $resp = $request.GetResponse();
-    $reqstream = $resp.GetResponseStream()
-    $sr = new-object System.IO.StreamReader $reqstream
-    while ($result = $sr.ReadLine()) {
-        #echo "$result $source $type"
+	#TLS v1.2 requires Powershell version3 
+	[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+	
+	$URI = "https://www.virusshare.com/hashes/VirusShare_$set.md5"
+	echo "Downloading file $URI"
+	$webpage = Invoke-WebRequest -Uri $URI -UseBasicParsing -TimeoutSec 600
+	echo "Download complete"
+	$webpage_lines = $webpage -split "`n"
+    
+	ForEach ($result in $webpage_lines) {
+        echo "$result $source $type"
         $cmd.commandtext = "INSERT INTO indicators (Hashes_MD5,Source,Type) VALUES('{0}','{1}','{2}')" -f $result,"$source - $set",$type
         $cmd.executenonquery()
     }
